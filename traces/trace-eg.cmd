9ef500371f9b46f7e2291718d4c98b8125317293
heisenberg0424 <agent.e@icloud.com>
2025-03-05 02:42:06 +0800

    Implement q_sort and q_merge
    Implement q_sort using merge sort and reused the merge function to
    implement q_merge.
    To be done: descending sort and lower the time complexity of q_merge
    
    Change-Id: I5f8f669c871249c5f020a37b6377b289a2766ea2

--- traces/trace-eg.cmd
+++ traces/trace-eg.cmd
@@ -1,22 +1,16 @@
-# Demonstration of queue testing framework
-# Use help command to see list of commands and options
-# Initial queue is NULL.
-show
-# Create empty queue
+option fail 0
+option malloc 0
 new
-# See how long it is
-size
-# Fill it with some values.  First at the head
-ih dolphin
-ih bear
-ih gerbil
-# Now at the tail
-it meerkat
-it bear
-# Reverse it
+ih a
+ih c
+ih e
+ih g
+ih b
+ih d
+ih f
+ih h
+sort
 reverse
-# See how long it is
-size
 # Delete queue.  Goes back to a NULL queue.
 free
 # Exit program
